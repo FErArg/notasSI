7b008a8cdcaa17467efde3fec07e1c42b89938ab
SerInformaticos <info@serinformaticos.es>
2012-02-07 09:28:29 +0100

    actualizada DB

---
+++ DB/notaSI+.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.7deb7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 07, 2012 at 09:32 AM
+-- Server version: 5.1.49
+-- PHP Version: 5.3.3-7+squeeze7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+
+-- www.SerInformaticos.es
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `notaSI+`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notas`
+--
+
+CREATE TABLE IF NOT EXISTS `notas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` int(11) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `titulo` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `texto` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `enlace` varchar(500) COLLATE utf8_spanish_ci NOT NULL,
+  `tag` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `eliminado` int(11) NOT NULL DEFAULT '0',
+  `visitas` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tag` (`tag`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(255) NOT NULL,
+  `clave` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
